0d1d04b6ecf023a1f8ed94b8c72b4e47cd39d202
Alessio Caiazza <alessio@chorally.com>
2017-01-23 00:47:18 +0100

    day2 step 2

--- lisp/day2.lisp
+++ lisp/day2.lisp
@@ -1,46 +1,59 @@
 ;; http://adventofcode.com/2016/day/2
 
-(defparameter *default-line-len* 3)
-(defparameter *default-limit* (* *default-line-len* *default-line-len*))
-(defparameter *default-position* 5)
+(defparameter *start1* '(1 . 1))
+(defparameter *map1* (make-array '(3 3)
+                              :initial-contents '((1 2 3) (4 5 6) (7 8 9))))
 
+(defparameter *start2* '(2 . 0))
+(defparameter *map2* (make-array '(5 5)
+                                 :initial-contents '(
+                                                     (() ()  1 () ())
+                                                     (()  2  3  4 ())
+                                                     ( 5  6  7  8  9)
+                                                     (()  A  B  C ())
+                                                     (() ()  D () ()))))
 
-(defun move (position direction line-len limit)
-  (let ((new-position (case direction
-                        (#\R (1+ position))
-                        (#\L (1- position))
-                        (#\U (- position line-len))
-                        (#\D (+ position line-len)))))
+(defun move (position direction map)
+  (let* ((limit (array-dimension map 0))
+         (row (car position))
+         (col (cdr position))
+         (new-position (case direction
+                        (#\R (cons row (1+ col)))
+                        (#\L (cons row (1- col)))
+                        (#\U (cons (1- row) col))
+                        (#\D (cons (1+ row) col))))
+         (new-row (car new-position))
+         (new-col (cdr new-position)))
     (cond
-      ((or (< new-position 1) (> new-position limit)) position)
-      ((and
-        (eq 1 (mod position line-len))
-        (eq #\L direction)) position)
-      ((and
-        (zerop (mod position line-len))
-        (eq #\R direction)) position)
+      ((or (< new-row 0) (< new-col 0)) position)
+      ((or (>= new-row limit) (>= new-col limit)) position)
+      ((not (aref map new-row new-col)) position)
       (t new-position))))
 
-(defun process-1-line (line &optional position line-len limit)
-  (unless position (setf position *default-position*))
-  (unless line-len (setf line-len *default-line-len*))
-  (unless limit (setf limit *default-limit*))
-
-  (reduce (lambda (acc elem)
-            (move acc elem line-len limit))
-          (string-trim (coerce (list #\Newline) 'string) line)
+(defun process-1-line (line map position)
+  (reduce (lambda (pos direction)
+            (move pos direction map))
+          line
           :initial-value position)
   )
+
+(defun solver (map lines start)
+  (reduce (lambda (acc line)
+            (let* ((key-cord (process-1-line line map acc))
+                   (row (car key-cord))
+                   (col (cdr key-cord))
+                   (key (aref map row col)))
+              (prin1 key)
+              key-cord))
+          lines :initial-value start))
 
 (defun main ()
   (let ((input (loop
                for line = (read-line nil nil 'eof)
                until (eq 'eof line)
                collect line)))
-    (reduce (lambda (acc line)
-              (let ((key (process-1-line line acc)))
-                (format t "~D <= ~S~%" key line)
-                key))
-            input :initial-value *default-position*)))
+    (solver *map1* input *start1*)
+    (princ #\Newline)
+    (solver *map2* input *start2*)))
 
 (main)
